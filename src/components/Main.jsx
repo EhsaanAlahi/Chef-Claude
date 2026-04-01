64a1374be890f10d7a1839b6a8749d66aa9de5d7
Ehsan Alahi <connectwithehsan@gmail.com>
2026-04-01 23:59:03 +0500

    ingredient information added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,7 @@
 export default function Main() {
+    const ingredients = ["potato" ,"tomato", "onion", "garlic", "carrot", "celery", "bellPepper", "spinach", "kale", "broccoli"];
+    const ingredientsList = ingredients.map(ingredient=> ( <li key={ingredient}>{ingredient}</li>));
+    
     return (
         <main>
             <form className="add-ingredient-form">
@@ -7,8 +10,9 @@ export default function Main() {
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
                 />
-                <button>Add ingredient</button>
+                <button onClick={e=>console.log("button clicked")}>Add ingredient</button>
             </form>
+                <ul>{ingredientsList}</ul>
         </main>
     )
 }
