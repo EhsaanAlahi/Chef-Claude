1d14103f534774941bdf8b6e5ec72608fdea5ee2
Ehsan Alahi <connectwithehsan@gmail.com>
2026-04-02 00:43:59 +0500

    added event to the form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,25 @@ export default function Main() {
     const ingredients = ["potato" ,"tomato", "onion", "garlic", "carrot", "celery", "bellPepper", "spinach", "kale", "broccoli"];
     const ingredientsList = ingredients.map(ingredient=> ( <li key={ingredient}>{ingredient}</li>));
     
+    function handleSubmit (event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient");
+        ingredients.push(newIngredient);
+        
+        
+
+    }
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name= "ingredient"
                 />
-                <button onClick={e=>console.log("button clicked")}>Add ingredient</button>
+                <button >Add ingredient</button>
             </form>
                 <ul>{ingredientsList}</ul>
         </main>
